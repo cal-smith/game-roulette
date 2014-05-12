f453b0ccf221f2951e876a3936e43cad5461127a
Callum Smith <hansolo669@users.noreply.github.com>
2014-05-12 16:18:59 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,29 @@
+		var games = [];
+		function add_games(){
+			var game = elem("game").value;
+			games.push(game);
+			elem("gamelist").innerHTML += '<div id="'+game+'"><span>[<a href="#" onclick="remove_game("'+game+'")">remove</a>]&nbsp;&nbsp;</span>'+game+'</div>';
+			elem("game").value="";
+		}
+		function remove_game(id){
+			var id = id.trim();
+			var index = games.indexOf(id);
+			console.log(index);
+			games.splice(index, 1);
+			elem("gamelist").removeChild(elem(id));
+		}
+		function decide(){
+			var max = games.length;
+			var random = Math.floor(Math.random()*max);
+			out.innerHTML = games[random];
+		}
+		document.addEventListener("DOMContentLoaded", function(event) {
+			elem("gameform").addEventListener("submit", function(event){
+				event.stopPropagation();
+				event.preventDefault();
+				add_games();
+			});
+		});
+		function elem(e){
+			return document.getElementById(e);
+		}
